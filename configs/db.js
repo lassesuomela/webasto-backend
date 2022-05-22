a19e751382f83903ab635d7756e888b749dde331
Lasse Suomela <suomelasse@gmail.com>
2022-05-23 00:21:56 +0300

    use mysql2 instead of mysql, changed tz from string to +3

--- configs/db.js
+++ configs/db.js
@@ -1,4 +1,4 @@
-let mysql = require('mysql');
+let mysql = require('mysql2');
 require('dotenv').config()
 
 const pool = mysql.createPool({
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    timezone: 'Europe/Helsinki'
+    timezone: '+03:00'
 })
 
 module.exports = pool;
