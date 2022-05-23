22b3b9e1975c6b775d718f98b95f066ce858bebf
Lasse Suomela <suomelasse@gmail.com>
2022-05-23 15:22:03 +0300

    modified timestamp addition on status update

--- models/statusModel.js
+++ models/statusModel.js
@@ -5,7 +5,7 @@ const status = {
         return db.query("SELECT * FROM statusData WHERE id = ?", [id], cb);
     },
     update: (data, cb) => {
-        return db.query("UPDATE statusData SET status = ?, onTime = ?, pulseSent = ? , rssi = ? WHERE id = ?", [data.newStatus, data.onTime, data.pulseSent, data.rssi, data.id], cb);
+        return db.query("UPDATE statusData SET status = ?, onTime = ?, pulseSent = ? , rssi = ? , timestamp = DATE_FORMAT(current_timestamp(), '%d.%c.%Y %H:%i') WHERE id = ?", [data.newStatus, data.onTime, data.pulseSent, data.rssi, data.id], cb);
     }
 }
 
