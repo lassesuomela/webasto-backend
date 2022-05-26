1b9401f7d4e40a701fcff7803af047a1576376ab
Lasse Suomela <suomelasse@gmail.com>
2022-05-26 16:14:02 +0300

    removed timezone from db.js, because it is no longer needed

--- configs/db.js
+++ configs/db.js
@@ -6,8 +6,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    timezone: '+03:00'
+    database: process.env.DB_DATABASE
 })
 
 module.exports = pool;
