0e343bf98b580382f4731204da2250b1709c32b4
Lasse Suomela <suomelasse@gmail.com>
2022-07-08 19:06:44 +0300

    added delete otp function where you are requied to authenticated last time with the otp, added some finnish error msgs, added delete route for deleting otp

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -17,6 +17,41 @@ const createSecret = (req, res) => {
     })
 }
 
+const deleteSecret = (req, res) => {
+
+    let otpCode = req.body.otp;
+
+    // fetch secret based on username
+    otp.getSecretByUsername(req.jwtUsername, (err, result) => {
+
+        if(err) {
+            console.log(err);
+            return res.status(500);
+        }
+
+        let verify = twofactor.verifyToken(result[0].secret, otpCode);
+
+        if(verify === null){
+            return res.status(401).json({status:"error",message:"Väärä OTP koodi"});
+        }else if(verify.delta === 0){
+
+            // save null as secret to db
+            otp.setSecretByUsername(null, req.jwtUsername, (err, result) => {
+                if(err) {
+                    console.log(err);
+                    return res.status(500);
+                }
+
+                res.json({status:"success",message:"OTP poistettu käytöstä"});
+            })
+
+            return res.json({status:"success",message:"OTP koodi hyväksytty"});
+        }else{
+            return res.status(401).json({status:"error",message:"Vanha OTP koodi"});
+        }
+    })
+}
+
 const createOTPCode = (req, res) => {
 
     // fetch secret from db based on username
@@ -28,10 +63,9 @@ const createOTPCode = (req, res) => {
         }
 
         if(result.length > 0){
-            const newToken = twofactor.generateToken(result[0].secret);
-            res.json(newToken)
+            res.json({status:"success",message:"OTP koodi generoitu onnistuneesti"})
         }else{
-            res.json({status:"error",message:"No secret found for username" + req.jwtUsername})
+            res.json({status:"error",message:"OTP:tä ei ole alustettu"})
         }
     })
 }
@@ -61,6 +95,7 @@ const verifyToken = (req, res) => {
 
 module.exports = {
     createSecret,
+    deleteSecret,
     createOTPCode,
     verifyToken
 }
--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -4,6 +4,7 @@ let router = express.Router();
 let otpController = require('../controllers/otpController');
 
 router.post('/secret', otpController.createSecret);
+router.delete('/secret', otpController.createSecret);
 router.get('/token', otpController.createOTPCode);
 router.post('/token', otpController.verifyToken);
 
