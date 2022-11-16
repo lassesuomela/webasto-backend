f26005b943c4a439e187ec8a713eac1f28ca252a
Lasse Suomela <suomelasse@gmail.com>
2022-11-16 13:29:15 +0200

    changed limit from 12 to 288 to match full day

--- models/voltageModel.js
+++ models/voltageModel.js
@@ -6,7 +6,7 @@ const voltage = {
         return db.query("SELECT * FROM voltageData ORDER BY id DESC LIMIT 1", cb);
     },
     getVoltageHour: (cb) => {
-        return db.query("SELECT * FROM voltageData WHERE id%5=0 ORDER BY id DESC LIMIT 12", cb);
+        return db.query("SELECT * FROM voltageData WHERE id%5=0 ORDER BY id DESC LIMIT 288", cb);
     },
     addVoltage: (voltage, cb) => {
         return db.query("INSERT INTO voltageData (voltage) VALUES (?)", [voltage], cb);
